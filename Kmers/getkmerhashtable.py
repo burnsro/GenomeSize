fbedbc332b8c49c3987cc8a8fa1a44dd34e99064
Robin Burns <burnsro@tcd.ie>
2021-08-02 20:47:08 +0200

    Add files via upload

---
+++ Kmers/getkmerhashtable.py
@@ -0,0 +1,45 @@
+from optparse import OptionParser                                                                                                    
+parser=OptionParser()
+parser.add_option('-r', '--ref', dest='ref', help='reference kmers', default='')
+parser.add_option("-a", "--acc", dest="acc", help="acc kmers", default="")
+(options, args)=parser.parse_args()
+
+import sys
+from Bio import SeqIO
+
+kmers_acc=set()
+for kmer in SeqIO.parse("%s"%options.acc, "fasta"):
+	kmers_acc.add(hash(str(kmer.seq)))
+
+
+#print(kmers_acc)
+#counter=0
+out=open('%s.shared'%options.ref, 'w')
+out2=open('%s.unique'%options.ref, 'w')
+kmers_ref=set()
+for record in SeqIO.parse('%s'%options.ref, "fasta"):
+	seq_hash = hash(str(record.seq))
+	kmers_ref.add(hash(str(record.seq)))
+	#print (seq_hash)
+	if seq_hash in kmers_acc:
+		out.write(str(record.seq)+ '\n')	
+	else:
+		out2.write(str(record.seq)+ '\n')
+
+#if (counter/len(kmers_ref)) >= 0.3:
+#	print("1")
+#else:
+#	print("0")
+
+#print(counter)
+
+
+
+
+
+
+
+
+
+
+
